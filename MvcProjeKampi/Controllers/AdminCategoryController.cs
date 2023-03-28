8729dc5aeed7f7b530a283eb9e72d9a583f4e715
Barış Bükümcüler <baris_bukumculer@hotmail.com>
2023-03-28 19:06:53 +0300

    changes

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,67 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var categoryvalues = categoryManager.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categortyvalidator=new CategoryValidator();
+            ValidationResult result=categortyvalidator.Validate(p);
+            if (result.IsValid) 
+            {
+                categoryManager.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage); 
+                }
+            }
+            return View();
+        }
+        public ActionResult DeleteCategory(int id) 
+        {
+            var values=categoryManager.GetByID(id);
+            categoryManager.CategoryDelete(values);
+            
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateCategory(int id) 
+        {
+            var categoryvalues=categoryManager.GetByID(id);
+            return View(categoryvalues);
+        }
+        [HttpPost]
+        public ActionResult UpdateCategory(Category p)
+        {
+            categoryManager.CategoryUpdate(p);
+         
+                return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/StatisticController.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class StatisticController : Controller
+    {
+        // GET: Statistic
+        public ActionResult Index()
+        {
+            Context context = new Context();
+            ViewBag.categoryCount = context.Categories.Count();
+            ViewBag.yazilimheadingCount = context.Headings.Where(c => c.Category.CategoryName=="Yazılım").Count();
+            ViewBag.AWriters =(from x in context.Writers where x.WriterName.Contains("a") select x).ToList().Count();
+
+            ViewBag.maxheadingCategory = context.Headings.OrderBy(x => x.CategoryID).GroupBy(y => y.Category.CategoryName).Select(z => new { KategoriAdı =z.Key,ToplamBaşlıkSayısı=z.Count()}).OrderByDescending(x=>x.ToplamBaşlıkSayısı).First();
+
+            ViewBag.falseandtruecategory = context.Categories.Where(x => x.CategoryStatus == true).Count() - context.Categories.Where(x => x.CategoryStatus == false).Count();
+           
+            return View();
+        }
+    }
+}
