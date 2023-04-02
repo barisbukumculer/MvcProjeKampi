b28d5696a994f8810f7b2d267748c643a03639bc
Barış Bükümcüler <baris_bukumculer@hotmail.com>
2023-04-02 15:57:19 +0300

    heading and writer crud

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,27 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        Context context = new Context();
+        public WriterValidator()
+        {
+            
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adı Boş olamaz.");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazarın Soyadı Boş olamaz.");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında Kısmını Boş bırakamazsınız.");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Ünvan Kısmını Boş bırakamazsınız.");
+            RuleFor(x=>x.WriterAbout).Matches("a"). WithMessage("Hakkında Kısmınında en az 1 adet a harfi içermelidir.");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girin.");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakter girin.");
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            List<SelectListItem> valuewriter = (from x in wm.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurname,
+                                                    Value = x.WriterID.ToString()
+                                                }).ToList();
+            ViewBag.vlw = valuewriter;
+            ViewBag.vlc = valuecategory;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAdd(heading);
+            return RedirectToAction("Index");
+        }
+    }
+}
